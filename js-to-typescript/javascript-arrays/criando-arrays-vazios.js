d9e2b58ccc1637a3931dee141fb2ac4576c5cab6
rhyanndev <rhyannjosebezerra@gmail.com>
2023-12-19 19:20:43 -0300

    encaminhando pasta do curso de Arrays em JavaScript

---
+++ js-to-typescript/javascript-arrays/criando-arrays-vazios.js
@@ -0,0 +1,12 @@
+const arrayVazia = [,,,];
+
+//criamos 3 posições, mas nenhuma delas existem um valor
+
+
+console.log(arrayVazia.length); // 3 elementos
+
+arrayVazia.push(50)
+
+console.log(arrayVazia.length); // 4 elementos
+
+// Array Esparso

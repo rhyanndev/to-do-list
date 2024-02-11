2aab0643fb6393675e94fa53982e1adc42673902
rhyanndev <rhyannjosebezerra@gmail.com>
2024-02-10 22:24:31 -0300

    criação do HTML e complemento do CSS

---
+++ Weather App/index.js
@@ -0,0 +1,32 @@
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', ()=>{
+    const APIKey = '';
+    const city = document.querySelector('.search-box input').value;
+
+    if(city === '')
+        return;
+
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`).then(response => response.json()).then
+        (json =>{
+
+            if(json.cod === '404'){
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'block';
+            error404.classList.remove('fadeIn');
+
+        })
+
+
+})
